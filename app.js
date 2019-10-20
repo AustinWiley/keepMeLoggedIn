71d539dc95402180e06ef30d88fc87055a081765
Austin WIley <wileybeer@gmail.com>
2019-10-20 11:51:40 -0600

    added click on interval

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 //app.js
-alert("Hello this will keep you logged into LMI. . . . .eventually");
+// alert("Hello this will keep you logged into LMI. . . . .eventually");
+// setInterval(function () {
+//     alert("Hello");
+// }, 3000);
+
+// window.onload = function () {
+//     document.getElementById("linkid").click();
+// };
+
+// document.getElementById("linkid").click();
+
+//   window.onload=function(){
+//     setInterval(function(){ alert("Hello"); }, 3000);
+//   };
+
+window.onload = function () {
+    setInterval(function () {
+        document.getElementById("navbarNav").click();
+        console.log("Click")
+    }, 3000);
+};
